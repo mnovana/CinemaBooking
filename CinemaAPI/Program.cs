332fdaea6ba0976ea6a48acb31d3ffb7db871723
mnovana <maravicnovana@gmail.com>
2024-10-30 01:45:58 +0100

    Add Ocelot API Gateway

---
+++ CinemaAPI/Program.cs
@@ -0,0 +1,58 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using SharedLibrary.Config;
+using System.Text;
+
+namespace CinemaAPI
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            EnvSetup.EnsureEnvFileExists();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+            // Add services to the container.
+            builder.Services.AddOcelot(builder.Configuration);
+            builder.Services.AddSwaggerForOcelot(builder.Configuration);
+            builder.Services.AddControllers();
+            // Authentication
+            var issuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
+            var audiences = Environment.GetEnvironmentVariable("JWT_AUDIENCE").Split(',');
+            var key = Environment.GetEnvironmentVariable("JWT_SIGNING_KEY");
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = issuer,
+                        ValidAudiences = audiences,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+                    };
+                });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
+            await app.UseOcelot();
+            app.Run();
+        }
+    }
+}

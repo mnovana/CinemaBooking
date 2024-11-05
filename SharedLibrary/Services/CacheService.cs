064d1424a942f6ea43d3debd1b5af45997821c6f
mnovana <maravicnovana@gmail.com>
2024-11-06 00:01:23 +0100

    Add CacheService

---
+++ SharedLibrary/Services/CacheService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Caching.Distributed;
+using SharedLibrary.Services.Interfaces;
+using System.Text.Json;
+
+namespace SharedLibrary.Services
+{
+    public class CacheService : ICacheService
+    {
+        public readonly IDistributedCache _cache;
+
+        public CacheService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+        
+        public async Task<T> GetData<T>(string key)
+        {
+            var value = await _cache.GetStringAsync(key);
+
+            if(!string.IsNullOrEmpty(value))
+            {
+                return JsonSerializer.Deserialize<T>(value);
+            }
+
+            return default;
+        }
+
+        public async Task RemoveData<T>(string key)
+        {
+            await _cache.RemoveAsync(key);
+        }
+
+        public async Task SetData<T>(string key, T value, DateTimeOffset expirationTime)
+        {
+            var cacheEntryOptions = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1)
+            };
+
+            await _cache.SetStringAsync(key, JsonSerializer.Serialize(value), cacheEntryOptions);
+        }
+    }
+}
---
+++ SharedLibrary/Services/Interfaces/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedLibrary.Services.Interfaces
+{
+    public interface ICacheService
+    {
+        Task<T> GetData<T>(string key);
+        Task SetData<T>(string key, T value, DateTimeOffset expirationTime);
+        Task RemoveData<T>(string key);
+    }
+}

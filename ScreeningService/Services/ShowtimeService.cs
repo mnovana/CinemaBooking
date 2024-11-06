bdd0d1f72c0d7fe8bde8ad5ed3c0c7e823579cc5
mnovana <maravicnovana@gmail.com>
2024-11-06 21:19:48 +0100

    Get/set movie cache on get, add, update showtime

--- ScreeningService/Services/ShowtimeService.cs
+++ ScreeningService/Services/ShowtimeService.cs
@@ -25,12 +25,12 @@ namespace ScreeningService.Services
         }
 
         public async Task<ShowtimeDTO> AddAsync(Showtime showtime)
-        {   
-            // fetch title and duration from FilmService
-            var movieDto = await GetMovieTitleDuration(showtime.MovieId);
+        {
+            // fetch title and duration from cache or FilmService
+            var movieTitleDurationDto = await GetMovieTitleDuration(showtime.MovieId);
 
             // calculate end and check overlapping
-            showtime.End = showtime.Start.AddMinutes(movieDto.Duration);
+            showtime.End = showtime.Start.AddMinutes(movieTitleDurationDto.Duration);
 
             if (await _showtimeRepository.ShowtimesOverlap(showtime))
             {
@@ -46,7 +46,7 @@ namespace ScreeningService.Services
             var showtimeDto = _mapper.Map<ShowtimeDTO>(addedShowtime);
 
             // set missing data fetched earlier from another db
-            showtimeDto.MovieTitle = movieDto.Title;
+            showtimeDto.MovieTitle = movieTitleDurationDto.Title;
 
             return showtimeDto;
         }
@@ -57,7 +57,7 @@ namespace ScreeningService.Services
             {
                 throw new Exception($"Bad request, showtime with ID={id} could not be deleted because at least one reservation uses it.");
             }
-            
+
             await _cacheService.RemoveDataAsync($"showtime-{id}");
             
             return await _showtimeRepository.DeleteAsync(id);
@@ -142,11 +142,11 @@ namespace ScreeningService.Services
 
         public async Task<ShowtimeDTO?> UpdateAsync(Showtime showtime)
         {
-            // fetch title and duration from FilmService
-            var movieDto = await GetMovieTitleDuration(showtime.MovieId);
+            // fetch title and duration from cache or FilmService
+            var movieTitleDurationDto = await GetMovieTitleDuration(showtime.MovieId);
 
             // calculate end and check overlapping
-            showtime.End = showtime.Start.AddMinutes(movieDto.Duration);
+            showtime.End = showtime.Start.AddMinutes(movieTitleDurationDto.Duration);
 
             if (await _showtimeRepository.ShowtimesOverlap(showtime))
             {
@@ -163,37 +163,88 @@ namespace ScreeningService.Services
             var showtimeDto = _mapper.Map<ShowtimeDTO>(updatedShowtime);
 
             // set missing data fetched earlier from another db
-            showtimeDto.MovieTitle = movieDto.Title;
+            showtimeDto.MovieTitle = movieTitleDurationDto.Title;
 
             return showtimeDto;
         }
 
         private async Task<MovieTitleDurationDTO> GetMovieTitleDuration(int id)
         {
-            var client = _httpClientFactory.CreateClient("FilmService");
-            var response = await client.GetAsync($"movies/titleduration/{id}");
+            // search movie in cache
+            var cachedMovieTitleDuration = await _cacheService.GetDataAsync<MovieTitleDurationDTO>($"movieTitleDuration-{id}");
 
-            if (!response.IsSuccessStatusCode)
+            // if not existent, fetch title and duration from FilmService
+            if (cachedMovieTitleDuration != null)
             {
-                throw new Exception($"Failed to fetch movie title with ID={id}, status code {response.StatusCode}.");
+                return cachedMovieTitleDuration;
             }
+            else
+            {
+                var client = _httpClientFactory.CreateClient("FilmService");
+                var response = await client.GetAsync($"movies/titleduration/{id}");
 
-            var content = await response.Content.ReadFromJsonAsync<MovieTitleDurationDTO>();
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Failed to fetch movie title with ID={id}, status code {response.StatusCode}.");
+                }
 
-            return content ?? throw new Exception($"Failed to read the response from {response.RequestMessage.RequestUri.ToString()}.");
+                var content = await response.Content.ReadFromJsonAsync<MovieTitleDurationDTO>();
+
+                if (content == null)
+                {
+                    throw new Exception($"Failed to read the response from {response.RequestMessage.RequestUri.ToString()}.");
+                }
+
+                // set cache
+                await _cacheService.SetDataAsync($"movieTitleDuration-{id}", content, DateTimeOffset.Now.AddHours(1));
+                
+                return content;
+            }
         }
 
         private async Task<List<MovieTitleDTO>> GetMovieTitlesByIds(int[] ids)
         {
-            var client = _httpClientFactory.CreateClient("FilmService");
-            var response = await client.GetAsync($"movies/titles?ids={string.Join(",", ids)}");
+            // search for titles in cache
+            var cachedMovieTitles = await GetCachedMovieTitlesByIdsAsync(ids);
 
-            if (!response.IsSuccessStatusCode)
+            var idsHashset = new HashSet<int>(ids);
+            var cachedIdsHashset = new HashSet<int>(cachedMovieTitles.Select(movie => movie.Id));
+            // missing ids
+            idsHashset.ExceptWith(cachedIdsHashset);
+
+            if (idsHashset.IsNullOrEmpty())
             {
-                throw new Exception($"Failed to fetch movie titles, status code {response.StatusCode}.");
+                return cachedMovieTitles;
             }
+            else
+            {
+                var client = _httpClientFactory.CreateClient("FilmService");
+                var response = await client.GetAsync($"movies/titles?ids={string.Join(",", idsHashset)}");
 
-            return await response.Content.ReadFromJsonAsync<List<MovieTitleDTO>>() ?? [];
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Failed to fetch movie titles, status code {response.StatusCode}.");
+                }
+
+                var fetchedMovieTitles = await response.Content.ReadFromJsonAsync<List<MovieTitleDTO>>() ?? [];
+
+                // set cache
+                var fetchedMovieTitlesDict = fetchedMovieTitles.ToDictionary(m => $"movieTitle-{m.Id}", m => m);
+                await _cacheService.SetMultipleDataAsync(fetchedMovieTitlesDict, DateTimeOffset.Now.AddHours(1));
+
+                // combine fetched and cached titles
+                fetchedMovieTitles.AddRange(cachedMovieTitles);
+
+                return fetchedMovieTitles;
+            }
+        }
+
+        private async Task<List<MovieTitleDTO>> GetCachedMovieTitlesByIdsAsync(int[] ids)
+        {
+            string[] keys = ids.Select(i => $"movieTitle-{i}").ToArray();
+            var cachedMovieTitles = await _cacheService.GetMultipleDataAsync<MovieTitleDTO>(keys);
+
+            return cachedMovieTitles.ToList();
         }
 
         private async Task<bool> ReservationWithShowtimeExistsAsync(int showtimeId)

812c3a382878078ba7f5dbd9208dc5e1ede64062
mnovana <maravicnovana@gmail.com>
2024-11-06 21:22:31 +0100

    Add get/set methods for multiple keys

--- SharedLibrary/Services/CacheService.cs
+++ SharedLibrary/Services/CacheService.cs
@@ -26,6 +26,23 @@ namespace SharedLibrary.Services
             return default;
         }
 
+        public async Task<IEnumerable<T>> GetMultipleDataAsync<T>(string[] keys)
+        {
+            var values = new List<T>();
+
+            foreach (var key in keys)
+            {
+                var value = await _cache.GetStringAsync(key);
+
+                if (!string.IsNullOrEmpty(value))
+                {
+                    values.Add(JsonSerializer.Deserialize<T>(value));
+                }
+            }
+
+            return values;
+        }
+
         public async Task RemoveDataAsync(string key)
         {
             await _cache.RemoveAsync(key);
@@ -39,6 +56,19 @@ namespace SharedLibrary.Services
             };
 
             await _cache.SetStringAsync(key, JsonSerializer.Serialize(value), cacheEntryOptions);
+        }
+
+        public async Task SetMultipleDataAsync<T>(Dictionary<string, T> entries, DateTimeOffset expirationTime)
+        {
+            var cacheEntryOptions = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = expirationTime
+            };
+
+            foreach(KeyValuePair<string, T> entry in entries)
+            {
+                await _cache.SetStringAsync(entry.Key, JsonSerializer.Serialize(entry.Value), cacheEntryOptions);
+            }
         }
     }
 }
--- SharedLibrary/Services/Interfaces/ICacheService.cs
+++ SharedLibrary/Services/Interfaces/ICacheService.cs
@@ -3,7 +3,9 @@
     public interface ICacheService
     {
         Task<T?> GetDataAsync<T>(string key);
+        Task<IEnumerable<T>> GetMultipleDataAsync<T>(string[] keys);
         Task SetDataAsync<T>(string key, T value, DateTimeOffset expirationTime);
+        Task SetMultipleDataAsync<T>(Dictionary<string, T> entries, DateTimeOffset expirationTime);
         Task RemoveDataAsync(string key);
     }
 }

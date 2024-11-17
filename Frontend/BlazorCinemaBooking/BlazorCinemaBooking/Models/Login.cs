dd8cf03de686f91e56ec6e46750512a261c8bfe6
mnovana <maravicnovana@gmail.com>
2024-11-17 22:32:58 +0100

    Add login page

---
+++ Frontend/BlazorCinemaBooking/BlazorCinemaBooking/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCinemaBooking.Models
+{
+    public class Login
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = String.Empty;
+        [Required]
+        public string Password { get; set; } = String.Empty;
+
+    }
+}

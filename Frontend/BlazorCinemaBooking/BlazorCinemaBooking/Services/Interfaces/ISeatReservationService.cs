91c3722ec74eca5ed6efdccc97fe78be5565b069
mnovana <maravicnovana@gmail.com>
2024-12-02 05:24:27 +0100

    Add SeatReservationService

---
+++ Frontend/BlazorCinemaBooking/BlazorCinemaBooking/Services/Interfaces/ISeatReservationService.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorCinemaBooking.Services.Interfaces
+{
+    public interface ISeatReservationService
+    {
+        Task AddReservationAsync(int showtimeId, string? userEmail, int[] seats);
+    }
+}
---
+++ Frontend/BlazorCinemaBooking/BlazorCinemaBooking/Services/SeatReservationService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorCinemaBooking.Services.Interfaces;
+
+namespace BlazorCinemaBooking.Services
+{
+    public class SeatReservationService : ISeatReservationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public SeatReservationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:5002");
+        }
+
+        public async Task AddReservationAsync(int showtimeId, string? userEmail, int[] seats)
+        {
+            var response = await _httpClient.PostAsJsonAsync("gateway/reservations", new { 
+                ShowtimeId = showtimeId, 
+                UserEmail = userEmail, 
+                ReservedSeats = seats 
+            });
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to add reservation, status code {response.StatusCode}.");
+            }
+        }
+    }
+}

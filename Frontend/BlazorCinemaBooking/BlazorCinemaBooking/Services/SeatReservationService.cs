15408712ebf261f16181fd5096cf27e22d579653
mnovana <maravicnovana@gmail.com>
2024-12-03 22:32:50 +0100

    Add local storage jwt to request

--- Frontend/BlazorCinemaBooking/BlazorCinemaBooking/Services/SeatReservationService.cs
+++ Frontend/BlazorCinemaBooking/BlazorCinemaBooking/Services/SeatReservationService.cs
@@ -1,23 +1,31 @@
 ﻿using BlazorCinemaBooking.Services.Interfaces;
+using Blazored.LocalStorage;
+using System.Net.Http.Headers;
 
 namespace BlazorCinemaBooking.Services
 {
     public class SeatReservationService : ISeatReservationService
     {
         private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorageService;
 
-        public SeatReservationService(HttpClient httpClient)
+        public SeatReservationService(HttpClient httpClient, ILocalStorageService localStorageService)
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri("https://localhost:5002");
+            _localStorageService = localStorageService;
         }
 
-        public async Task AddReservationAsync(int showtimeId, string? userEmail, int[] seats)
+        public async Task AddReservationAsync(int showtimeId, string userEmail, int[] seats)
         {
+            var token = await _localStorageService.GetItemAsStringAsync("jwt_token");
+            token = token.Trim('"');
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            
             var response = await _httpClient.PostAsJsonAsync("gateway/reservations", new { 
                 ShowtimeId = showtimeId, 
                 UserEmail = userEmail, 
-                ReservedSeats = seats 
+                ReservedSeats = seats.Select(seat => new { seatId = seat }).ToArray() 
             });
 
             if (!response.IsSuccessStatusCode)
